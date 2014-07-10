6d81a2c28f281a2e2d9b5b63eb19f5dd2ead4fde
Vitor Coimbra <vitorcoliveira91@gmail.com>
2014-07-10 17:37:53 -0300

    Method for validation of an entity

--- src/ecs/System.h
+++ src/ecs/System.h
@@ -10,14 +10,21 @@
 
 #include <memory>
 #include "Aspect.h"
+#include "Entity.h"
 
 class System {
 private:
 	AspectPtr root_aspect;
 
 public:
-	System(Aspect* root_aspect) : root_aspect(AspectPtr(root_aspect)) {}
+	System(Aspect* root_aspect) :
+			root_aspect(AspectPtr(root_aspect)) {
+	}
 	virtual ~System();
+
+	bool validate(const Entity& e) {
+		return root_aspect->validate(e);
+	}
 };
 
 #endif /* SYSTEM_H_ */
