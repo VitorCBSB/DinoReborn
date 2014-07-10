c9f82f6221bba20747a2207205b3ec701840cf23
Vitor Coimbra <vitorcoliveira91@gmail.com>
2014-07-10 15:54:01 -0300

    ECS files added to specific folder.

---
+++ src/ecs/Aspect.cpp
@@ -0,0 +1,18 @@
+/*
+ * Aspect.cpp
+ *
+ *  Created on: 10/07/2014
+ *      Author: vitor
+ */
+
+#include "Aspect.h"
+
+Aspect::Aspect() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Aspect::~Aspect() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/ecs/Aspect.h
@@ -0,0 +1,17 @@
+/*
+ * Aspect.h
+ *
+ *  Created on: 10/07/2014
+ *      Author: vitor
+ */
+
+#ifndef ASPECT_H_
+#define ASPECT_H_
+
+class Aspect {
+public:
+	Aspect();
+	virtual ~Aspect();
+};
+
+#endif /* ASPECT_H_ */
