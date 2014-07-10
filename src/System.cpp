deed5ca2eb7deb70e52843f2440be91c7cd5c240
Vitor Coimbra <vitorcoliveira91@gmail.com>
2014-07-10 15:48:53 -0300

    Base System class.

---
+++ src/System.cpp
@@ -0,0 +1,18 @@
+/*
+ * System.cpp
+ *
+ *  Created on: 10/07/2014
+ *      Author: vitor
+ */
+
+#include "System.h"
+
+System::System() {
+	// TODO Auto-generated constructor stub
+
+}
+
+System::~System() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/System.h
@@ -0,0 +1,17 @@
+/*
+ * System.h
+ *
+ *  Created on: 10/07/2014
+ *      Author: vitor
+ */
+
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+
+class System {
+public:
+	System();
+	virtual ~System();
+};
+
+#endif /* SYSTEM_H_ */
