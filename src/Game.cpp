afb60c0b6be6542cf0db878de0ef7132873d090e
Vitor Coimbra <vitorcoliveira91@gmail.com>
2014-07-10 15:43:06 -0300

    Added main class. Removed unnecesary file.

---
+++ src/Game.cpp
@@ -0,0 +1,19 @@
+/*
+ * Game.cpp
+ *
+ *  Created on: 10/07/2014
+ *      Author: vitor
+ */
+
+#include "Game.h"
+
+Game::Game() {
+
+}
+
+Game::~Game() {
+}
+
+int main() {
+
+}
---
+++ src/Game.h
@@ -0,0 +1,17 @@
+/*
+ * Game.h
+ *
+ *  Created on: 10/07/2014
+ *      Author: vitor
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+
+class Game {
+public:
+	Game();
+	virtual ~Game();
+};
+
+#endif /* GAME_H_ */
