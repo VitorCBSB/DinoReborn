2ccb111d5222ee28c7105f60d9dd3a2fdd06470f
Vitor Coimbra <vitorcoliveira91@gmail.com>
2014-12-15 09:15:31 -0200

    Does not attempt to rotate bullets with zero velocity anymore.

--- src/systems/BulletRotationSystem.cpp
+++ src/systems/BulletRotationSystem.cpp
@@ -21,5 +21,9 @@ void BulletRotationSystem::process_entity(Entity& entity, double dt) {
 	auto animation = entity.get_component<AnimationComponent>();
 	auto velocity = entity.get_component<VelocityComponent>();
 
+	if (velocity->velocity.length() == 0) {
+		return;
+	}
+
 	animation->animation.set_angle(velocity->velocity.angle());
 }
