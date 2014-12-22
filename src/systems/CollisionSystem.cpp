009fdc53f0f0fa469426e3d2fa804fdaa52a672c
Vitor Coimbra <vitorcoliveira91@gmail.com>
2014-12-22 09:51:33 -0200

    Execute collisions is now a templated method that receives the Collision Type to broadcast if a collision is detected.

--- src/systems/CollisionSystem.cpp
+++ src/systems/CollisionSystem.cpp
@@ -15,24 +15,7 @@ void CollisionSystem::process_entities(double dt) {
 			world_ptr.lock()->get_group_manager().get_entities_from_group(
 					"enemies");
 
-	execute_collisions(bullets, enemies,
-			[&](EntityPtr bullet, EntityPtr enemy) {
-				world_ptr.lock()->get_event_manager().broadcast<PlayerBulletEnemyCollision>(
-						bullet, enemy);
-			});
-}
-
-void CollisionSystem::execute_collisions(EntityMap& a_map, EntityMap& b_map,
-		const CollisionHandler& collision_handler) {
-	for (auto a_it = a_map.begin(); a_it != a_map.end(); a_it++) {
-		auto a = a_it->second;
-		for (auto b_it = b_map.begin(); b_it != b_map.end(); b_it++) {
-			auto b = b_it->second;
-			if (check_collision(*a, *b)) {
-				collision_handler(a, b);
-			}
-		}
-	}
+	execute_collisions<PlayerBulletEnemyCollision>(bullets, enemies);
 }
 
 bool CollisionSystem::check_collision(Entity& a, Entity& b) {
--- src/systems/CollisionSystem.h
+++ src/systems/CollisionSystem.h
@@ -19,8 +19,21 @@ typedef std::function<void(EntityPtr a, EntityPtr b)> CollisionHandler;
 class CollisionSystem: public System {
 private:
 	bool check_collision(Entity& a, Entity& b);
-	void execute_collisions(EntityMap& a_map, EntityMap& b_map,
-			const CollisionHandler& collision_handler);
+
+	template <typename CollisionType>
+	void execute_collisions(EntityMap& a_map, EntityMap& b_map) {
+		for (auto a_it = a_map.begin(); a_it != a_map.end(); a_it++) {
+			auto a = a_it->second;
+			for (auto b_it = b_map.begin(); b_it != b_map.end(); b_it++) {
+				auto b = b_it->second;
+				if (check_collision(*a, *b)) {
+					world_ptr.lock()->get_event_manager().broadcast<CollisionType>(
+							a, b);
+				}
+			}
+		}
+
+	}
 
 public:
 	CollisionSystem(WorldPtr world_ptr) :
