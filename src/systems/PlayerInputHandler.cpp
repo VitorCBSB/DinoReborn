2b869f625f35d3482fdcccccf87f846c36eb9437
Vitor Coimbra <vitorcoliveira91@gmail.com>
2015-01-02 10:13:33 -0200

    Animation stops correctly if player stops moving.

--- src/systems/PlayerInputHandler.cpp
+++ src/systems/PlayerInputHandler.cpp
@@ -14,7 +14,10 @@ void PlayerInputHandler::handle(const PlayerMoveLeft& event) {
 	}
 
 	auto v = player->get_component<VelocityComponent>();
+	auto anim = player->get_component<AnimationComponent>();
+
 	v->velocity += Vector2(-1, 0) * SPEED;
+	anim->animation.animation_state = 2;
 }
 
 void PlayerInputHandler::handle(const PlayerStopLeft& event) {
@@ -24,7 +27,12 @@ void PlayerInputHandler::handle(const PlayerStopLeft& event) {
 	}
 
 	auto v = player->get_component<VelocityComponent>();
+	auto anim = player->get_component<AnimationComponent>();
+
 	v->velocity -= Vector2(-1, 0) * SPEED;
+	if (v->velocity.length() == 0) {
+		anim->animation.animation_state = 0;
+	}
 }
 
 void PlayerInputHandler::handle(const PlayerMoveRight& event) {
@@ -34,7 +42,10 @@ void PlayerInputHandler::handle(const PlayerMoveRight& event) {
 	}
 
 	auto v = player->get_component<VelocityComponent>();
+	auto anim = player->get_component<AnimationComponent>();
+
 	v->velocity += Vector2(1, 0) * SPEED;
+	anim->animation.animation_state = 3;
 }
 
 void PlayerInputHandler::handle(const PlayerStopRight& event) {
@@ -44,7 +55,12 @@ void PlayerInputHandler::handle(const PlayerStopRight& event) {
 	}
 
 	auto v = player->get_component<VelocityComponent>();
+	auto anim = player->get_component<AnimationComponent>();
+
 	v->velocity -= Vector2(1, 0) * SPEED;
+	if (v->velocity.length() == 0) {
+		anim->animation.animation_state = 0;
+	}
 }
 
 void PlayerInputHandler::handle(const PlayerMoveForward& event) {
@@ -54,7 +70,10 @@ void PlayerInputHandler::handle(const PlayerMoveForward& event) {
 	}
 
 	auto v = player->get_component<VelocityComponent>();
+	auto anim = player->get_component<AnimationComponent>();
+
 	v->velocity += Vector2(0, -1) * SPEED;
+	anim->animation.animation_state = 1;
 }
 
 void PlayerInputHandler::handle(const PlayerStopForward& event) {
@@ -64,7 +83,12 @@ void PlayerInputHandler::handle(const PlayerStopForward& event) {
 	}
 
 	auto v = player->get_component<VelocityComponent>();
+	auto anim = player->get_component<AnimationComponent>();
+
 	v->velocity -= Vector2(0, -1) * SPEED;
+	if (v->velocity.length() == 0) {
+		anim->animation.animation_state = 0;
+	}
 }
 
 void PlayerInputHandler::handle(const PlayerMoveBackward& event) {
@@ -74,7 +98,10 @@ void PlayerInputHandler::handle(const PlayerMoveBackward& event) {
 	}
 
 	auto v = player->get_component<VelocityComponent>();
+	auto anim = player->get_component<AnimationComponent>();
+
 	v->velocity += Vector2(0, 1) * SPEED;
+	anim->animation.animation_state = 4;
 }
 
 void PlayerInputHandler::handle(const PlayerStopBackward& event) {
@@ -84,7 +111,12 @@ void PlayerInputHandler::handle(const PlayerStopBackward& event) {
 	}
 
 	auto v = player->get_component<VelocityComponent>();
+	auto anim = player->get_component<AnimationComponent>();
+
 	v->velocity -= Vector2(0, 1) * SPEED;
+	if (v->velocity.length() == 0) {
+		anim->animation.animation_state = 0;
+	}
 }
 
 void PlayerInputHandler::handle(const PlayerShoot& event) {
