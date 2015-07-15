64b539fb344d87eee41c6ac7015ad9e04ae1c96b
Vitor Coimbra <vitorcoliveira91@gmail.com>
2015-07-14 23:50:29 -0300

    Added more markers.

---
+++ src/components/marker/BackgroundMarker.h
@@ -0,0 +1,16 @@
+/*
+ * BackgroundMarker.h
+ *
+ *  Created on: 14/07/2015
+ *      Author: vitor
+ */
+
+#ifndef COMPONENTS_MARKER_BACKGROUNDMARKER_H_
+#define COMPONENTS_MARKER_BACKGROUNDMARKER_H_
+
+#include "../../ecs/VECS.h"
+
+class BackgroundMarker : public Component<BackgroundMarker> {
+};
+
+#endif /* COMPONENTS_MARKER_BACKGROUNDMARKER_H_ */
---
+++ src/components/marker/EnemyMarker.h
@@ -0,0 +1,16 @@
+/*
+ * EnemyMarker.h
+ *
+ *  Created on: 14/07/2015
+ *      Author: vitor
+ */
+
+#ifndef COMPONENTS_MARKER_ENEMYMARKER_H_
+#define COMPONENTS_MARKER_ENEMYMARKER_H_
+
+#include "../../ecs/VECS.h"
+
+class EnemyMarker : public Component<EnemyMarker> {
+};
+
+#endif /* COMPONENTS_MARKER_ENEMYMARKER_H_ */
